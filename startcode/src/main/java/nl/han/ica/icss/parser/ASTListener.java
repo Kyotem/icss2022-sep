e3ed795283c3f325dd2516d8e32ea56b13e84597
Kyotem <150026629+Kyotem@users.noreply.github.com>
2025-10-27 15:23:51 +0100

    Parser improvements & Comment clarity

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -1,10 +1,6 @@
 	package nl.han.ica.icss.parser;
 
-	import java.util.Stack;
-
-
 	import nl.han.ica.datastructures.HANStack;
-	import nl.han.ica.datastructures.IHANStack;
 	import nl.han.ica.icss.ast.*;
 	import nl.han.ica.icss.ast.literals.*;
 	import nl.han.ica.icss.ast.operations.AddOperation;
@@ -13,8 +9,6 @@
 	import nl.han.ica.icss.ast.selectors.ClassSelector;
 	import nl.han.ica.icss.ast.selectors.IdSelector;
 	import nl.han.ica.icss.ast.selectors.TagSelector;
-	import org.antlr.v4.runtime.ParserRuleContext;
-	import org.antlr.v4.runtime.Token;
 
 	/**
 	 * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
@@ -22,15 +16,15 @@
 	public class ASTListener extends ICSSBaseListener {
 
 		//Accumulator attributes:
-		private AST ast;
+		private final AST ast;
 
 		//Use this to keep track of the parent nodes when recursively traversing the ast
-		private IHANStack<ASTNode> currentContainer;
+		private final HANStack<ASTNode> currentContainer;
 
 		public ASTListener() {
 			ast = new AST();
 
-			currentContainer = new HANStack();
+			currentContainer = new HANStack<>();
 		}
 
 		// NOTE: Currently I have to typecast on every exit event to the type of node it should be,
@@ -44,7 +38,7 @@
 
 		@Override
 		public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
-			// FIXME: Is typecasting required? Is there a good way to convert or put it in directly? (Right now gonna be typecasting, will improve later if possible)
+			// Q: Is typecasting required? Is there a good way to convert or put it in directly? | A: Not specifically because they already are part of an ASTNode, might be good to typecast? Eh, check attachLatestOnStackToParent(); for more info.
 			ast.setRoot( (Stylesheet) currentContainer.pop() );
 		}
 
@@ -55,9 +49,9 @@
 			if (ctx.LOWER_IDENT() != null) {
 
 				String selectorString = ctx.LOWER_IDENT().getText();
-				Selector selector = null;
+				Selector selector;
 
-				// Check for selector type
+                // Check for selector type
 				if (selectorString.startsWith("#")) {
 					selector = new IdSelector(selectorString);
 				} else if (selectorString.startsWith(".")) {
@@ -78,12 +72,16 @@
 
 		@Override
 		public void enterPropertyexpr(ICSSParser.PropertyexprContext ctx) {
-			String propertyName = null; // FIXME: Need a nullcheck
+			String propertyName = null;
 
 			if (ctx.COLOR_PROPERTY() != null) {
 				propertyName = ctx.COLOR_PROPERTY().getText();
 			} else if (ctx.DIM_PROPERTY() != null) {
 				propertyName = ctx.DIM_PROPERTY().getText();
+			}
+
+			if(propertyName == null) { // NOTE: This should NOT happen, but adding a check to prevent application from entering a bad state.
+				throw new IllegalStateException("propertyName from a PropertyExpression is NULL");
 			}
 
 			Declaration declaration = new Declaration(propertyName);
@@ -95,7 +93,7 @@
 			attachLatestOnStackToParent();
 		}
 
-		// NOTE: Colorvalue is handled as a separated parser rule, dimensional values is handled under 'factor' per parser.
+		// NOTE: Color value is handled as a separated parser rule, dimensional values is handled under 'factor' per parser.
 		@Override
 		public void enterColorValue(ICSSParser.ColorValueContext ctx) {
 			ASTNode value = null;
@@ -125,8 +123,13 @@
 			VariableReference varRef = new VariableReference(ctx.CAPITAL_IDENT().getText());
 			variableAssignment.addChild(varRef);
 
-			// FIXME: Separation of concern is not handled properly (Don't want ot handle this here because it will result in duplicate & pot inefficient code) -> Look into this when also fixing math expressions
-			if (ctx.BOOLEAN() != null) {
+			/*
+			NOTE: There is no separation of concerns here; it does not make sense to handle BOOLEAN value like so separately here.
+			Would be better to have a parser rule for that literal specifically and push, pop, and add as child via the other methods.
+			Not implementing this anymore due to time-constraints, but would be a smart improvement for readability.
+
+			 */
+			  if (ctx.BOOLEAN() != null) {
 				BoolLiteral boolValue = new BoolLiteral(ctx.BOOLEAN().getText());
 				variableAssignment.addChild(boolValue);
 			}
@@ -138,7 +141,7 @@
 			attachLatestOnStackToParent();
 		}
 
-		// if - else statements
+		// If - Else statements
 		@Override
 		public void enterIfstmt(ICSSParser.IfstmtContext ctx) {
 
@@ -186,13 +189,6 @@
 			}
 		}
 
-
-		/* FIXME: This is stupid.
-		Practically duplicating a lot of checking code, either separate concerns per parser rules (abstract more)
-		Or introduce helper functions.
-
-		Not to mention this breaks the entry-exit structure on the stack. Not adhering to pattern and likely not flexible.
-		 */
 		@Override
 		public void enterFactor(ICSSParser.FactorContext ctx) {
 			currentContainer.push(buildLiteralFromContext(ctx));
@@ -215,10 +211,9 @@
 			return null;
 		}
 
-
 		// Currently only implemented on 'factor', can this be used on other places?
 		// Can't use ParserRuleContext + getToken because it would result in walking the parse tree multiple times (Not fast!)
-		// NOTE: So using it only for factorcontext, calling an extra function takes up more stack space, but is more readable!
+		// NOTE: So using it only for FactorContext, calling an extra function takes up more stack space, but is more readable!
 		private ASTNode buildLiteralFromContext(ICSSParser.FactorContext ctx) {
 			if (ctx == null) return null;
 
@@ -235,13 +230,12 @@
 			return null;
 		}
 
-		// Doing this so I don't have to typecast in every entry/exit conditions to centralize it a bit more.
-		// This means I won't do specific typecasting to classes that extend ASTNode (etc), so it might make it less readable, but this significantly reduces code count.
+		// Using a separate function to centralize this code.
+		// I won't do specific typecasting for the type of node, but can be done with if-instanceof checks, but adding it as is, should work fine.
 		private void attachLatestOnStackToParent() {
-			ASTNode node = (ASTNode) currentContainer.pop();
+			ASTNode node = currentContainer.pop();
 			currentContainer.peek().addChild(node);
 		}
-
 
 		public AST getAST() {
 			return ast;
